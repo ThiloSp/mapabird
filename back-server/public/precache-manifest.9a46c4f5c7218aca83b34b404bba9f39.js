8522506e64c7a8f1b62694e31eb83ef5e278c105
Thilo Sperber <t.sperber@gmx.net>
2019-02-28 17:37:34 +0100

    build moved

---
+++ back-server/public/precache-manifest.9a46c4f5c7218aca83b34b404bba9f39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c431c22c041db4aa6113",
+    "url": "/static/js/main.c431c22c.chunk.js"
+  },
+  {
+    "revision": "79c1442ec8120cddd45d",
+    "url": "/static/js/2.79c1442e.chunk.js"
+  },
+  {
+    "revision": "c431c22c041db4aa6113",
+    "url": "/static/css/main.fa0091b4.chunk.css"
+  },
+  {
+    "revision": "f4398dde9df6849110508ab36e935185",
+    "url": "/index.html"
+  }
+];
