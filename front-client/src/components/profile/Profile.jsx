cebe959153559de902369f406e9e55e53c1e918b
Thilo Sperber <t.sperber@gmx.net>
2019-03-02 16:14:45 +0100

    uploaded pic shows in profile but doesnt save

--- front-client/src/components/profile/Profile.jsx
+++ front-client/src/components/profile/Profile.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AuthService from "../authentification/auth-service";
 import AddProfilePic from "./addProfilePic";
+import ProfileService from "./profile-service";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = { loggedInUser: null };
     this.service = new AuthService();
+    this.profileService = new ProfileService();
   }
 
   componentDidMount() {
@@ -18,12 +20,22 @@ class Profile extends Component {
     this.setState({ ...this.state, loggedInUser: nextProps["userInSession"] });
   }
 
-  /* logoutUser = () => {
-    this.service.logout().then(() => {
-      this.setState({ loggedInUser: null });
-      this.props.getUser(null);
-    });
-  }; */
+  handleFileUpload = e => {
+    console.log("The file to be uploaded is: ", e.target.files[0]);
+    const uploadData = new FormData();
+    uploadData.append("photoPath", e.target.files[0]);
+    this.profileService
+      .handleUpload(uploadData)
+      .then(response => {
+        console.log("response is: ", response);
+        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
+        this.setState({...this.state, loggedInUser: {photoPath: response.secure_url}});
+        console.log(this.state)
+      })
+      .catch(err => {
+        console.log("Error while uploading the file: ", err);
+      });
+  };
 
   render() {
     if (this.state.loggedInUser) {
@@ -31,7 +43,8 @@ class Profile extends Component {
         <div>
           <h2>Welcome, {this.state.loggedInUser.username}!</h2>
           <img src={this.state.loggedInUser.photoPath} alt="" />
-          <AddProfilePic userInSession={this.state.loggedInUser} />
+          {/* <AddProfilePic userInSession={this.state.loggedInUser} /> */}
+          <AddProfilePic userInSession={this.state.loggedInUser} handleChange={this.handleFileUpload}/>
         </div>
       );
     } else {
--- front-client/src/components/profile/addProfilePic.jsx
+++ front-client/src/components/profile/addProfilePic.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import ProfileService from "./profile-service";
+/* import ProfileService from "./profile-service"; */
 
 export default class AddProfilePic extends Component {
-  constructor(props) {
+ /*  constructor(props) {
     super(props);
     this.state = { loggedInUser: null };
     this.profileService = new ProfileService();
@@ -14,30 +14,16 @@ export default class AddProfilePic extends Component {
 
   componentWillReceiveProps(nextProps) {
     this.setState({ ...this.state, loggedInUser: nextProps["userInSession"] });
-  }
+  } */
 
-  handleFileUpload = e => {
-    console.log("The file to be uploaded is: ", e.target.files[0]);
-    const uploadData = new FormData();
-    uploadData.append("photoPath", e.target.files[0]);
-    this.profileService
-      .handleUpload(uploadData)
-      .then(response => {
-        console.log("response is: ", response);
-        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
-        this.setState({ photoPath: response.secure_url });
-      })
-      .catch(err => {
-        console.log("Error while uploading the file: ", err);
-      });
-  };
   render() {
     return (
       <div>
         <h2>Change Profile Pic</h2>
         <form>
           <label>Upload your photo</label>
-          <input type="file" onChange={e => this.handleFileUpload(e)} />
+          {/* <input type="file" onChange={e => this.handleFileUpload(e)} /> */}
+          <input type="file" onChange={e => this.props.handleChange(e)} />
         </form>
       </div>
     );
