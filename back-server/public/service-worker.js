cdbcf96197940dbd8d2b48086e9de1f3f8b2f875
Thilo Sperber <t.sperber@gmx.net>
2019-03-07 09:37:37 +0100

    heroku

--- back-server/public/service-worker.js
+++ back-server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2defe7d32b16a42a90d3978ab558eb8.js"
+  "/precache-manifest.84cdadc877fa460274a10b55fe5b28ca.js"
 );
 
 workbox.clientsClaim();
