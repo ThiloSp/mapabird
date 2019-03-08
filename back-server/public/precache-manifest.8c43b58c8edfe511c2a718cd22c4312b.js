4e6a750cd7c9ecd2e38752e9d17484a74657f2a3
Thilo Sperber <t.sperber@gmx.net>
2019-03-08 05:36:48 +0100

    deploy

---
+++ back-server/public/precache-manifest.8c43b58c8edfe511c2a718cd22c4312b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b3383a5322ca070ddfd4",
+    "url": "/static/js/main.bef49875.chunk.js"
+  },
+  {
+    "revision": "d14da06a4ed5bf6503a5",
+    "url": "/static/js/2.25364868.chunk.js"
+  },
+  {
+    "revision": "b3383a5322ca070ddfd4",
+    "url": "/static/css/main.2f9e2d90.chunk.css"
+  },
+  {
+    "revision": "d14da06a4ed5bf6503a5",
+    "url": "/static/css/2.68650f0b.chunk.css"
+  },
+  {
+    "revision": "29418d58681826b4f444265fccb88a99",
+    "url": "/index.html"
+  }
+];
