5f4569a515bd20e9eb850ee7d16f7cf16e253e2b
Thilo Sperber <t.sperber@gmx.net>
2019-03-26 17:59:56 +0100

    title changed

---
+++ back-server/public/precache-manifest.a7be27fbdf12e08060fd8f81f60edfa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e3ed45a959dbee87e5a7",
+    "url": "/static/js/main.fc106a9f.chunk.js"
+  },
+  {
+    "revision": "1fca941c6d27c664fb38",
+    "url": "/static/js/2.371d89f6.chunk.js"
+  },
+  {
+    "revision": "e3ed45a959dbee87e5a7",
+    "url": "/static/css/main.900b62e1.chunk.css"
+  },
+  {
+    "revision": "1fca941c6d27c664fb38",
+    "url": "/static/css/2.68650f0b.chunk.css"
+  },
+  {
+    "revision": "5ac9738a9aea1810c0cd4b9d9c0557c8",
+    "url": "/index.html"
+  }
+];
